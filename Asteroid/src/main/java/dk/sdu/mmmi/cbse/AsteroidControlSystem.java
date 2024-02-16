0db87940d4951db862763b7957acfdad170d5c0a
Zigordion <sigurdion@live.dk>
2024-02-16 09:28:04 +0100

    c

--- Asteroid/src/main/java/dk/sdu/mmmi/cbse/AsteroidControlSystem.java
+++ Asteroid/src/main/java/dk/sdu/mmmi/cbse/AsteroidControlSystem.java
@@ -1,4 +1,19 @@
 package dk.sdu.mmmi.cbse;
 
-public class AsteroidControlSystem {
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+
+public class AsteroidControlSystem implements IEntityProcessingService {
+asdasd
+    @Override
+    public void process(double deltaTime, GameData gameData, World world) {
+        timer -= 1; //should include delta time
+        if (timer <= 0) {
+            Entity enemy = createEnemy(gameData);
+            world.addEntity(enemy);
+            timer = maxTimer;
+        }
+    }
 }
--- Enemy/src/main/java/dk/sdu/mmmi/cbse/EnemyControlSystem.java
+++ Enemy/src/main/java/dk/sdu/mmmi/cbse/EnemyControlSystem.java
@@ -18,47 +18,46 @@ public class EnemyControlSystem implements IEntityProcessingService {
     private static double shootTimer;
     private final double maxShootTimer = 20;
     private final double rotationSpeed = 5;
-    private final double moveSpeed =1;
+    private final double moveSpeed = 1;
     Random random = new Random();
+
     @Override
     public void process(double deltaTime, GameData gameData, World world) {
-        //spawn Enemy at random position along edge of map
-        //set forward to be equal to player position at spawn of enemy
-        //move enemy forward
-        timer-=1;
-        shootTimer-= random.nextDouble();
-        if(timer<=0){
+        timer -= 1; //should include delta time
+        shootTimer -= random.nextDouble();
+        if (timer <= 0) {
             Entity enemy = createEnemy(gameData);
             world.addEntity(enemy);
             timer = maxTimer;
         }
-        for (Entity enemy : world.getEntities(Enemy.class) ) {
+        for (Entity enemy : world.getEntities(Enemy.class)) {
             double changeX = Math.cos(Math.toRadians(enemy.getRotation()));
             double changeY = Math.sin(Math.toRadians(enemy.getRotation()));
-            enemy.setX(enemy.getX() + changeX*deltaTime*moveSpeed);
-            enemy.setY(enemy.getY() + changeY*deltaTime*moveSpeed);
-            enemy.setRotation(enemy.getRotation() + random.nextDouble(-rotationSpeed,rotationSpeed)*deltaTime);
-            if(enemy.getY() >= gameData.getDisplayHeight() || enemy.getY() <= 0){
+            enemy.setX(enemy.getX() + changeX * deltaTime * moveSpeed);
+            enemy.setY(enemy.getY() + changeY * deltaTime * moveSpeed);
+            enemy.setRotation(enemy.getRotation() + random.nextDouble(-rotationSpeed, rotationSpeed) * deltaTime);
+            if (enemy.getY() >= gameData.getDisplayHeight() || enemy.getY() <= 0) {
                 enemy.setActive(false);
             }
-            if(enemy.getX() >= gameData.getDisplayWidth() || enemy.getX() <= 0){
+            if (enemy.getX() >= gameData.getDisplayWidth() || enemy.getX() <= 0) {
                 enemy.setActive(false);
             }
-            if(shootTimer<=0){
+            if (shootTimer <= 0) {
                 getBulletSPIs().stream().findFirst().ifPresent(
                         spi -> world.addEntity(spi.createBullet(enemy, gameData))
                 );
             }
         }
-        if(shootTimer<= 0){
-            shootTimer=maxShootTimer;
+        if (shootTimer <= 0) {
+            shootTimer = maxShootTimer;
         }
     }
-    public Entity createEnemy(GameData gameData){
+
+    public Entity createEnemy(GameData gameData) {
         Entity enemy = new Enemy();
-        enemy.setPolygonCoordinates(-5,-5  ,10,5,  -5,5,  10,-5);
-        enemy.setX(random.nextDouble(5, gameData.getDisplayWidth()-5));
-        enemy.setY(random.nextDouble(5, gameData.getDisplayHeight()-5));
+        enemy.setPolygonCoordinates(-5, -5, 10, 5, -5, 5, 10, -5);
+        enemy.setX(random.nextDouble(5, gameData.getDisplayWidth() - 5));
+        enemy.setY(random.nextDouble(5, gameData.getDisplayHeight() - 5));
         enemy.setActive(true);
         return enemy;
     }
