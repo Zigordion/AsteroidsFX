01c8dc828e1772a682f2e82d78fcc31fb14b4c85
Zigordion <sigurdion@live.dk>
2024-02-19 10:24:26 +0100

    a

--- Asteroid/src/main/java/dk/sdu/mmmi/cbse/AsteroidControlSystem.java
+++ Asteroid/src/main/java/dk/sdu/mmmi/cbse/AsteroidControlSystem.java
@@ -6,14 +6,19 @@ import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 
 public class AsteroidControlSystem implements IEntityProcessingService {
-asdasd
+    private static double timer;
     @Override
     public void process(double deltaTime, GameData gameData, World world) {
         timer -= 1; //should include delta time
         if (timer <= 0) {
-            Entity enemy = createEnemy(gameData);
-            world.addEntity(enemy);
-            timer = maxTimer;
+            Entity asteroid = createAsteroid(gameData,world);
         }
+    }
+
+    private Entity createAsteroid(GameData gameData, World world){
+
+
+
+        return new Asteroid();
     }
 }
